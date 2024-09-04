5572d0530450e05cbc19a74ffd4d2b220c6536bb
felixmathew03 <felixmathewtt@gmail.com>
2024-09-04 09:26:09 +0530

    success

--- clientside/js/index.js
+++ clientside/js/index.js
@@ -1,1 +1,41 @@
-alert("HAii")
+async function getdonors() {
+    const res=await fetch("http://localhost:3000/getdonors");
+    const data=await res.json();
+    str=``;
+    data.map((dt)=>{
+        str+=`
+        <div class="content" >
+            <input type="text" value=${dt.name} disabled=true name="name" id="name" placeholder="Name">
+            <input type="email" value=${dt.email}  disabled=true name="email" id="email" placeholder="Email">
+            <input type="text" value=${dt.phone}  disabled=true name="pnp" id="pno" placeholder="Phone no.">
+            <input type="text"  value=${dt.bgrop}  disabled=true name="bgrp" id="bgrp" placeholder="Blood Group">
+            <input type="text" value=${dt.gender}  disabled=true name="gender" id="gender" placeholder="Gender">
+            <div class="butn">
+                <button class="edit" > Edit</button>
+                <button class="save" >Save</button>
+                <button class="delete" onclick="handleDelete('${dt._id}')" >Delete</button>
+            </div>
+        </div>    
+        `
+    });
+    document.getElementById("main").innerHTML=str
+}
+
+getdonors();
+
+async function handleDelete(id){
+    const res=await fetch("http://localhost:3000/delete",{
+        method:"DELETE",
+        headers:{"Content-Type":"text/plain"},
+        "body":id
+    });
+    console.log(res);
+    const data= await res.text();
+    if(data=="success"){
+        alert("successfully deleted")
+        getdonors();
+    }
+    else{
+        alert("Deletion Failed")
+    }
+}
--- serverside/server.js
+++ serverside/server.js
@@ -1,12 +1,13 @@
+const PORT=3000;
 const http=require("http")
 const fs=require("fs")
 const url=require("url")
 const queryString=require("querystring")
-const {MongoClient}=require("mongodb")
+const {MongoClient,ObjectId}=require("mongodb")
 //connect database
 const client=new MongoClient("mongodb://127.0.0.1:27017/")
 
-const app=http.createServer((req,res)=>{
+const app=http.createServer(async(req,res)=>{
     //create database
     const db=client.db("BloodDonors")
     //create collection
@@ -48,7 +49,40 @@ const app=http.createServer((req,res)=>{
         res.writeHead(200,{"Content-Type":"text/html"});
         res.end(fs.readFileSync("../clientside/index.html"))
     }
+
+    if(path.pathname=="/getdonors" && req.method=="GET"){
+        const data = await collection.find().toArray();
+        const jsonData=JSON.stringify(data);
+        console.log(jsonData);
+        res.writeHead(200,{"Content-Type":"text/json"});
+        res.end(jsonData)
+    }
+    if(path.pathname=="/delete" && req.method=="DELETE"){
+        console.log("reached delete route");
+        let body="";
+        req.on("data",(chunks)=>{
+            body+=chunks.toString();
+            console.log(body);
+        })
+        req.on("end",async()=>{
+            let _id=new ObjectId(body);
+            console.log(_id);
+            collection.deleteOne({_id}).then(()=>{
+                res.writeHead(200,{"Content-Type":"text/plain"});
+                res.end("success")
+            }).catch(()=>{
+                res.writeHead(200,{"Content-Type":"text/plain"});
+                res.end("fail")
+            })
+        })
+    }
+});
+
+client.connect().then(()=>{
+    console.log("database connected");
+    app.listen(PORT,()=>{
+        console.log(`server at http://localhost:${PORT}/`);
+    });
+}).catch((error)=>{
+    console.log(error);
 })
-
-
-app.listen(3000);
